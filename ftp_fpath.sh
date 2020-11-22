77a88871f93fb596558141f6ef285ee4f6fc52dc
Francisco Lima <franciscolima@Franciscos-iMac-Pro.local>
2020-11-22 17:08:01 +0100

    update CDF import

--- ftp_fpath.sh
+++
@@ -1,1 +0,0 @@
-wget ftp://ftp.ebi.ac.uk/pub/databases/metabolights/studies/public/MTBLS528/\*.cdf -P data/
--- script.py
+++ script.py
@@ -1,23 +1,39 @@
+#%% Import some modules
 
-# Import some modules
+import os, glob
+os.chdir('/Users/franciscolima/Documents/Projects/metab_conv/')
 import pandas as pd
+import numpy as np
 #import netCDF4 as nc
-from pyms.GCMS.IO. import JCAMP_reader
+from matplotlib import pyplot as plt
+from skimage import io
+from pyms.GCMS.IO.ANDI import ANDI_reader
+# TODO: Check diff between int_matrix and int_matrix_i
+from pyms.IntensityMatrix import build_intensity_matrix
 
-# Try example 
-fpath = '/Users/franciscolima/Documents/metab_conv/data/9_3.cdf'
-ds = nc.Dataset(fpath)
-ds
+#%% Download CDFs from Tohge's study
 
-#%%
-for var in ds.variables.values():
-    print(var)
+ftp_url = 'ftp://ftp.ebi.ac.uk/pub/databases/metabolights/studies/public/MTBLS528/\*.cdf'
+# Put wget command together, download to data/
+cmd = 'wget ' + ftp_url + ' -P data/'
+# Initiate download process
+if not os.path.exists('data/'):
+    os.system(cmd) # NOT RUN
 
-test = ds['intensity_values'][:]
-# %%
-#from pyms.GCMS.IO.JCAMP import JCAMP_reader
+#%% Try example 
 
-#data = JCAMP_reader(jcamp_file)
+# TODO: Work on baseline correction
+fpaths = glob.glob('data/*.cdf')
 
-#from pyms.IntensityMatrix import build_intensity_matrix_i
-#im = build_intensity_matrix_i(data)
+#ds = nc.Dataset(fpath)
+dat = ANDI_reader(fpaths[10])
+im = build_intensity_matrix(dat) # (nscan, nm/z) ~ (4401, 1901)
+log_im = np.log(im.intensity_array+1)
+# Visualize
+plt.imshow(log_im)
+ 
+#%% use skimage.io to export im
+
+os.mkdir('images/')
+# Avoid lossy compression!
+io.imsave('images/' + f + '.png', arr=im)
