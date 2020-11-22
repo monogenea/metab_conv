b99a746edd7a7cc11477b6f23efc889f2c5393e3
Francisco Lima <franciscolima@Franciscos-MacBook-Pro.local>
2020-11-22 14:31:19 +0100

    netCDF4 import

--- script.py
+++ script.py
@@ -1,4 +1,23 @@
 
 # Import some modules
 import pandas as pd
+#import netCDF4 as nc
+from pyms.GCMS.IO. import JCAMP_reader
 
+# Try example 
+fpath = '/Users/franciscolima/Documents/metab_conv/data/9_3.cdf'
+ds = nc.Dataset(fpath)
+ds
+
+#%%
+for var in ds.variables.values():
+    print(var)
+
+test = ds['intensity_values'][:]
+# %%
+#from pyms.GCMS.IO.JCAMP import JCAMP_reader
+
+#data = JCAMP_reader(jcamp_file)
+
+#from pyms.IntensityMatrix import build_intensity_matrix_i
+#im = build_intensity_matrix_i(data)
